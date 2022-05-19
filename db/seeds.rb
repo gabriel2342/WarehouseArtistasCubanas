b1914579af0d4276c07d3eec81064d0a6deced58
Gabriel Mariano Pendas <90109448+gabriel2342@users.noreply.github.com>
2022-05-19 00:56:59 +0000

    added views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Warehouse.create(name: 'Habana')
+Warehouse.create(name: 'Holguin')
+Warehouse.create(name: 'Velasco')
+Warehouse.create(name: 'Santiago')
+Warehouse.create(name: 'Cienfuegos')
+
+
+Item.create(artist: 'Zilia Sanchez Dominguez', name: 'Maquinista, dipitico', quantity: 23, warehouse_id: 2 )
+Item.create(artist: 'Gina Pellon', name: 'Insulas al pairo', quantity: 23, warehouse_id: 5 )
+Item.create(artist: 'Sandra Ramos', name: 'La maldita circumstancia', quantity: 7, warehouse_id: 3 )
+Item.create(artist: 'Ana Albertina Delgado', name: 'Cosiendo mi corazon', quantity: 42, warehouse_id: 4 )
+Item.create(artist: 'Antonia Eiriz', name: 'Vigilia', quantity: 23, warehouse_id: 3 )
+Item.create(artist: 'Elsa Mora', name: 'Mirar al vacio', quantity: 7, warehouse_id: 1 )
+Item.create(artist: 'Kcho', name: 'Coluna infinita', quantity: 42, warehouse_id: 1 )
